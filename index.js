e614540eac39cf8ecb40d0d1cfd267d7ed00fa2b
ChaseTeague <teaguechase@yahoo.com>
2023-12-09 11:46:55 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 /* Your Code Here */
+function createEmployeeRecord(employee){
+    return {
+        firstName : employee[0],
+        familyName : employee[1],
+        title : employee[2],
+        payPerHour : employee[3],
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+}
 
+function createEmployeeRecords(records){
+    return records.map(data => createEmployeeRecord(data));
+}
+
+function createTimeInEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour), 
+        date
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    let inTime = this.timeInEvents.find(e => e.date === date)
+    let outTime = this.timeOutEvents.find(e => e.date === date)
+    return (outTime.hour - inTime.hour) / 100
+}
+
+function wagesEarnedOnDate(dateForm){
+    let pay = hoursWorkedOnDate.call(this, dateForm) * this.payPerHour
+    return pay
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +65,10 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(record => record.firstName === firstName)
+}
+
+function calculatePayroll(records){
+    return records.reduce((accum, init) => accum + allWagesFor.call(init), 0)
+}
